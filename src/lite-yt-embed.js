c25557b36764c1203c56d2747ea333299aa21cc0
Paweł Kowalski <pavelloz@gmail.com>
2020-11-18 00:33:53 +0100

    Improvements, cleanups

--- src/lite-yt-embed.js
+++ src/lite-yt-embed.js
@@ -14,30 +14,21 @@ class LiteYTEmbed extends HTMLElement {
     constructor() {
         super();
 
-        // Gotta encode the untrusted value
-        // https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#rule-2---attribute-escape-before-inserting-untrusted-data-into-html-common-attributes
-        this.videoId = encodeURIComponent(this.getAttribute('videoid'));
+        this.videoId = encodeURIComponent(this.getAttribute("videoid"));
 
-        /**
-         * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
-         * There is much internet debate on the reliability of thumbnail URLs. Weak consensus is that you
-         * cannot rely on anything and have to use the YouTube Data API.
-         *
-         * amp-youtube also eschews using the API, so they just try sddefault with a hqdefault fallback:
-         *   https://github.com/ampproject/amphtml/blob/6039a6317325a8589586e72e4f98c047dbcbf7ba/extensions/amp-youtube/0.1/amp-youtube.js#L498-L537
-         * For now I'm gonna go with this confident (lol) assertion: https://stackoverflow.com/a/20542029, though I'll use `i.ytimg` to optimize for origin reuse.
-         *
-         * Worth noting that sddefault is _higher_ resolution than hqdefault. Naming is hard. ;)
-         * From my own testing, it appears that hqdefault is ALWAYS there sddefault is missing for ~10% of videos
-         *
-         * TODO: Do the sddefault->hqdefault fallback
-         *       - When doing this, apply referrerpolicy (https://github.com/ampproject/amphtml/pull/3940)
-         * TODO: Consider using webp if supported, falling back to jpg
-         */
+        let customPoster = this.getAttribute("poster");
+
+        if (window.WebP) {
+          customPoster.replace(/\.(jpg|jpeg)$/, '.webp');
+        }
+
+        this.posterUrl =
+          customPoster ||
+          `https://i.ytimg.com/vi/${this.videoId}/maxresdefault.jpg`;
+
         this.posterUrl = `https://i.ytimg.com/vi/${this.videoId}/hqdefault.jpg`;
         // Warm the connection for the poster image
         LiteYTEmbed.addPrefetch('preload', this.posterUrl, 'image');
-        // TODO: support dynamically setting the attribute via attributeChangedCallback
     }
 
     connectedCallback() {
@@ -56,13 +47,6 @@ class LiteYTEmbed extends HTMLElement {
         this.addEventListener('click', e => this.addIframe());
     }
 
-    // // TODO: Support the the user changing the [videoid] attribute
-    // attributeChangedCallback() {
-    // }
-
-    /**
-     * Add a <link rel={preload | preconnect} ...> to the head
-     */
     static addPrefetch(kind, url, as) {
         const linkElem = document.createElement('link');
         linkElem.rel = kind;
@@ -70,31 +54,14 @@ class LiteYTEmbed extends HTMLElement {
         if (as) {
             linkElem.as = as;
         }
-        linkElem.crossOrigin = 'anonymous';
         document.head.append(linkElem);
     }
 
-    /**
-     * Begin pre-connecting to warm up the iframe load
-     * Since the embed's network requests load within its iframe,
-     *   preload/prefetch'ing them outside the iframe will only cause double-downloads.
-     * So, the best we can do is warm up a few connections to origins that are in the critical path.
-     *
-     * Maybe `<link rel=preload as=document>` would work, but it's unsupported: http://crbug.com/593267
-     * But TBH, I don't think it'll happen soon with Site Isolation and split caches adding serious complexity.
-     */
     static warmConnections() {
         if (LiteYTEmbed.preconnected) return;
 
         // The iframe document and most of its subresources come right off youtube.com
         LiteYTEmbed.addPrefetch('preconnect', 'https://www.youtube-nocookie.com');
-        // The botguard script is fetched off from google.com
-        LiteYTEmbed.addPrefetch('preconnect', 'https://www.google.com');
-
-        // Not certain if these ad related domains are in the critical path. Could verify with domain-specific throttling.
-        LiteYTEmbed.addPrefetch('preconnect', 'https://googleads.g.doubleclick.net');
-        LiteYTEmbed.addPrefetch('preconnect', 'https://static.doubleclick.net');
-
         LiteYTEmbed.preconnected = true;
     }
 
